86b8cc7a589b1790b86b05458a46ba8e444a7bc6
Mitesh Agrawal <miteshagrawal@hotmail.com>
2021-06-27 21:10:24 +0100

    First commit

---
+++ MVCEF/Models/PartialClasses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCEF.Models
+{
+    [MetadataType(typeof(DepartmentMetaData))]
+    public partial class Department
+    {
+
+    }
+
+    public class DepartmentMetaData
+    {
+        [Display(Name = "Department Name")]
+        public string Name { get; set; }
+    }
+}
---
+++ MVCEF/Models/PartialEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCEF.Models
+{
+    [MetadataType(typeof(EmployeeMetaData))]
+    public partial class Employee
+    {
+    }
+
+    public partial class EmployeeMetaData
+    {
+        [Required]
+        public string DOB { get; set; }
+        [Required]
+        [Display(Name ="Department")]
+        public int Department_Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        //public string Address { get; set; }
+        //public string City { get; set; }
+
+        //public virtual Department Department { get; set; }
+    }
+}
